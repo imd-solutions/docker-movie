0984ff9b5be3591d6a3188dc084e40dd9fadce36
Oladipo George <oladipo.george@erlang-solutions.com>
2023-08-03 08:26:28 +0100

    json-server added

--- frontend/src/utils/Apollo.ts
+++ frontend/src/utils/Apollo.ts
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
+import { setContext } from '@apollo/client/link/context';
 
 
 const errorLink = onError(({ graphqlErrors}) => {
@@ -16,8 +17,20 @@ const link = from([
     new HttpLink({uri: 'http://localhost/graphql'})
 ])
 
+const authLink = setContext((_, { headers }) => {
+    
+    let token = JSON.parse(localStorage.getItem("authUser") || "{}");
+        
+    return {
+      headers: {
+        ...headers,
+        authorization: token !== undefined ? `Bearer ${token.access_token}` : "",
+      }
+    }
+  });
+
 const client = new ApolloClient({
-link: link,
+link: authLink.concat(link),
 cache: new InMemoryCache(),
 });
 
