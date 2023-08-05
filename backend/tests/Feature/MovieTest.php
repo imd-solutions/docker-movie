349014c47633f4eb3bf017009217f373f3e7bd97
Oladipo George <oladipo.george@erlang-solutions.com>
2023-08-05 13:15:03 +0100

    Use Http::fake for movie feature test

--- backend/tests/Feature/MovieTest.php
+++ backend/tests/Feature/MovieTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
 use MarvinRabe\LaravelGraphQLTest\TestGraphQL;
 use Tests\Fragments;
 
@@ -26,16 +27,29 @@ class MovieTest extends TestCase
      */
     public function canAddNewMovieViaApi()
     {
+        Http::fake([
+            'http://192.168.1.169:4000/movie_quotes' => Http::response([
+                "*" => [
+                    'quote' => $this->faker->sentence,
+                    'movie' => $this->faker->word,
+                    'year' => $this->faker->year,
+                    'image_url' => $this->faker->imageUrl,
+                    'rating' => $this->faker->numberBetween(1, 5),
+                    'character' => $this->faker->name
+                ]
+            ], 200),
+        ]);
+
         $response = $this->json('get', '/movies')
         ->assertStatus(200)
         ->assertJsonStructure(
             [
                 '*' => [
-                    "id",
                     "quote" ,
                     "movie",
                     "year",
                     "image_url",
+                    "rating",
                     "character"
                 ]
             ]
