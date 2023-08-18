65ff6cc7a7ed2d6487b8ebf719bc90238a52de1c
Oladipo George <oladipo.george@erlang-solutions.com>
2023-08-18 19:30:01 +0100

    json server

--- backend/app/Http/Controllers/MovieController.php
+++ backend/app/Http/Controllers/MovieController.php
@@ -10,7 +10,7 @@ class MovieController extends Controller
 {
     public function getMovieData() {
 
-        $response = Http::get('http://192.168.1.169:4000/movie_quotes');
+        $response = Http::get(config('app.json'));
 
         $jsonData = $response->json();
 
--- backend/config/app.php
+++ backend/config/app.php
@@ -55,6 +55,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'json' => env('JSON_SERVER', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
